fe38c2fe1530b9a91d49549d1f57645767a18e00
mjsdev5 <mcbridejames196@gmail.com>
2022-03-15 17:16:45 +0200

    fixing the shadows

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -35,7 +35,11 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    // overflow: "hidden", This is for Android but takes the shadow of for IOS
+    elevation: 3,
+    overflow:
+      Platform.OS === "android" && Platform.Version >= 21
+        ? "hidden"
+        : "visible",
   },
   container: {
     flex: 1,
@@ -44,7 +48,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.6,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     justifyContent: "flex-end",
     alignItems: "flex-end",
     padding: 15,
