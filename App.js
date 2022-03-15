7c3c68f44fdd07c8fe7c3d23f202bd614ba8fad8
mjsdev5 <mcbridejames196@gmail.com>
2022-03-15 10:27:09 +0200

    adding fonts & apploading component

--- App.js
+++ App.js
@@ -1,6 +1,28 @@
+import { useState } from "react";
 import { View, Text } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
     <View>
       <Text>Universal React with Expo</Text>
