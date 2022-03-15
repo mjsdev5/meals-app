13f7d3d7d3673c8cc35072c616ab24ab1c8c7bb0
mjsdev5 <mcbridejames196@gmail.com>
2022-03-15 17:13:27 +0200

    adding header buttons

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
-  ImageBackground
-} from 'react-native';
+  ImageBackground,
+} from "react-native";
 
-const MealItem = props => {
+const MealItem = (props) => {
   return (
     <View style={styles.mealItem}>
       <TouchableOpacity onPress={props.onSelectMeal}>
@@ -38,39 +38,40 @@ const MealItem = props => {
 const styles = StyleSheet.create({
   mealItem: {
     height: 200,
-    width: '100%',
-    backgroundColor: '#f5f5f5',
+    width: "100%",
+    backgroundColor: "#f5f5f5",
     borderRadius: 10,
-    overflow: 'hidden'
+    overflow: "hidden",
+    marginVertical: 10,
   },
   bgImage: {
-    width: '100%',
-    height: '100%',
-    justifyContent: 'flex-end',
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end",
   },
   mealRow: {
-    flexDirection: 'row'
+    flexDirection: "row",
   },
   mealHeader: {
-    height: '85%'
+    height: "85%",
   },
   mealDetail: {
     paddingHorizontal: 10,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: '15%'
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "15%",
   },
   titleContainer: {
-    backgroundColor: 'rgba(0,0,0,0.5)',
+    backgroundColor: "rgba(0,0,0,0.5)",
     paddingVertical: 5,
-    paddingHorizontal: 12
+    paddingHorizontal: 12,
   },
   title: {
-    fontFamily: 'open-sans-bold',
+    fontFamily: "open-sans-bold",
     fontSize: 20,
-    color: 'white',
-    textAlign: 'center'
-  }
+    color: "white",
+    textAlign: "center",
+  },
 });
 
 export default MealItem;
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
-import { MEALS } from '../data/dummy-data';
+import { MEALS } from "../data/dummy-data";
+import HeaderButton from "../components/HeaderButton";
 
-const MealDetailScreen = props => {
-  const mealId = props.navigation.getParam('mealId');
+const MealDetailScreen = (props) => {
+  const mealId = props.navigation.getParam("mealId");
 
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
     <View style={styles.screen}>
@@ -21,20 +23,31 @@ const MealDetailScreen = props => {
   );
 };
 
-MealDetailScreen.navigationOptions = navigationData => {
-  const mealId = navigationData.navigation.getParam('mealId');
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return {
-    headerTitle: selectedMeal.title
+    headerTitle: selectedMeal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("Mark as favorite!");
+          }}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default MealDetailScreen;
