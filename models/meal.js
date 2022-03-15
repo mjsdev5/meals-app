b4a0165b3c7d5f746813b553a65e751a58264635
mjsdev5 <mcbridejames196@gmail.com>
2022-03-15 12:58:42 +0200

    adding meal model & data

---
+++ models/meal.js
@@ -0,0 +1,33 @@
+class Meal {
+  constructor(
+    id,
+    categoryIds,
+    title,
+    affordability,
+    complexity,
+    imageUrl,
+    duration,
+    ingredients,
+    steps,
+    isGlutenFree,
+    isVegan,
+    isVegeterian,
+    isLactoseFree
+  ) {
+    this.id = id;
+    this.categoryIds = categoryIds;
+    this.title = title;
+    this.ingredients = ingredients;
+    this.steps = steps;
+    this.duration = duration;
+    this.complexity = complexity;
+    this.affordability = affordability;
+    this.imageUrl = imageUrl;
+    this.isGlutenFree = isGlutenFree;
+    this.isVegan = isVegan;
+    this.isVegeterian = isVegeterian;
+    this.isLactoseFree = isLactoseFree;
+  }
+}
+
+export default Meal;
