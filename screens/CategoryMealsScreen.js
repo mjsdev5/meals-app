6c049afe68b57b82788730d8168f8cad7bd6814b
mjsdev5 <mcbridejames196@gmail.com>
2022-03-15 14:31:04 +0200

    passing data to the meal detail screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 
-import { CATEGORIES, MEALS } from '../data/dummy-data';
-import MealItem from '../components/MealItem';
+import { CATEGORIES, MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
-const CategoryMealScreen = props => {
-  const renderMealItem = itemData => {
+const CategoryMealScreen = (props) => {
+  const renderMealItem = (itemData) => {
     return (
       <MealItem
         title={itemData.item.title}
@@ -13,15 +13,22 @@ const CategoryMealScreen = props => {
         duration={itemData.item.duration}
         complexity={itemData.item.complexity}
         affordability={itemData.item.affordability}
-        onSelectMeal={() => {}}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+            routeName: "MealDetail",
+            params: {
+              mealId: itemData.item.id,
+            },
+          });
+        }}
       />
     );
   };
 
-  const catId = props.navigation.getParam('categoryId');
+  const catId = props.navigation.getParam("categoryId");
 
   const displayedMeals = MEALS.filter(
-    meal => meal.categoryIds.indexOf(catId) >= 0
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
   );
 
   return (
@@ -30,29 +37,29 @@ const CategoryMealScreen = props => {
         data={displayedMeals}
         keyExtractor={(item, index) => item.id}
         renderItem={renderMealItem}
-        style={{ width: '100%' }}
+        style={{ width: "100%" }}
       />
     </View>
   );
 };
 
-CategoryMealScreen.navigationOptions = navigationData => {
-  const catId = navigationData.navigation.getParam('categoryId');
+CategoryMealScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam("categoryId");
 
-  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
 
   return {
-    headerTitle: selectedCategory.title
+    headerTitle: selectedCategory.title,
   };
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 15
-  }
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+  },
 });
 
 export default CategoryMealScreen;
