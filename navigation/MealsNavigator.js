76d10468c8d6661547d64b485ce82e945298d7b8
mjsdev5 <mcbridejames196@gmail.com>
2022-03-16 09:36:30 +0200

    more navigation config & styling

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,4 +1,4 @@
-import { Platform } from "react-native";
+import { Platform, Text } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
@@ -15,6 +15,12 @@ import Colors from "../constants/Colors";
 const defaultStackNavOptions = {
   headerStyle: {
     backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+  },
+  headerTitleStyle: {
+    fontFamily: "open-sans-bold",
+  },
+  headerBackTitleStyle: {
+    fontFamily: "open-sans",
   },
   headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
   headerTitle: "A Screen",
@@ -57,6 +63,12 @@ const tabScreenConfig = {
         );
       },
       tabBarColor: Colors.primaryColor,
+      tabBarLabel:
+        Platform.OS === "android" ? (
+          <Text style={{ fontFamily: "open-sans-bold" }}>Meals</Text>
+        ) : (
+          "Meals"
+        ),
     },
   },
   Favorites: {
@@ -77,10 +89,19 @@ const MealsFavTabNavigator =
         shifting: true,
         barStyle: {
           backgroundColor: Colors.primaryColor,
+          tabBarLabel:
+            Platform.OS === "android" ? (
+              <Text style={{ fontFamily: "open-sans-bold" }}>Favorites</Text>
+            ) : (
+              "Favorites"
+            ),
         },
       })
     : createBottomTabNavigator(tabScreenConfig, {
         tabBarOptions: {
+          labelStyle: {
+            fontFamily: "open-sans-bold",
+          },
           activeTintColor: Colors.accentColor,
         },
       });
